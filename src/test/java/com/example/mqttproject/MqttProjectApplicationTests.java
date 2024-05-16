2895d9a072f7c64953a40137c19f9e5f55a1b4b0
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2024-05-16 16:02:37 -0400

    first commit

---
+++ src/test/java/com/example/mqttproject/MqttProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqttproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
